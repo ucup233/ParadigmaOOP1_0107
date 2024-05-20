831bb3dd7a956efcb9acf8cac5e958feffd0b5d8
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-05-20 14:48:08 +0700

    pendifinisian prosedur void dari member class pasien dan dokter

--- ContohAssociation/ContohAssociation.cpp
+++ ContohAssociation/ContohAssociation.cpp
@@ -8,10 +8,10 @@ public:
 	string nama;
 	vector<dokter*> daftar_dokter;
 	pasien(string pNama) :nama(pNama) {
-		cout << "Pasien \"" << nama"\"";
+		cout << "Pasien \"" << nama<<"\" ada \n";
 	}
 	~pasien() {
-		cout << "Pasien \"" << nama << "\"";
+		cout << "Pasien \"" << nama << "\" tidak ada \n";
 	}
 
 	void tambahDokter(dokter*);
@@ -31,3 +31,28 @@ public:
 	void tambahPasien(pasien*);
 	void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+	daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakDokter() {
+	cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+	for (auto& a : daftar_dokter) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+
+void dokter::tambahPasien(pasien* pPasien) {
+	daftar_pasien.push_back(pPasien);
+	pPasien->tambahDokter(this);
+}
+
+void dokter::cetakPasien() {
+	cout << "Daftar Pasien dari dokter\"" << this->nama << "\":\n";
+	for (auto& a : daftar_pasien) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
