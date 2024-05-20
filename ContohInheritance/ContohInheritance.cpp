b0ba661a494b4862917bee0eec6fc3b9d4b84ec5
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-05-20 14:57:28 +0700

    class orang sebagai base class

---
+++ ContohInheritance/ContohInheritance.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+	int jumlah(int a, int b) {
+		return a + b;
+	}
+};
