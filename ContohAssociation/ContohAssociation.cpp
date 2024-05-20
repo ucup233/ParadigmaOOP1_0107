35406dc2c54152369aff2115b75d703beb1ed167
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-05-20 15:11:44 +0700

    finishing

--- ContohAssociation/ContohAssociation.cpp
+++ ContohAssociation/ContohAssociation.cpp
@@ -23,10 +23,10 @@ public:
 	vector<pasien*> daftar_pasien;
 
 	dokter(string pNama) :nama(pNama) {
-		cout << "Dokter \"" << nama << "\"";
+		cout << "Dokter \"" << nama << "\" ada \n";
 	}
 	~dokter() {
-		cout << "Dokter \"" << nama << "\"";
+		cout << "Dokter \"" << nama << "\" tidak ada \n";
 	}
 	void tambahPasien(pasien*);
 	void cetakPasien();
